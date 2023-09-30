2108e80f47b66c858ef68a4b67466a5e07993542
felipe de oliveira <felipeoliveira2f@gmail.com>
2023-09-29 22:13:57 -0300

    Modificando arquivos de execucao

--- christian.sh
+++ christian.sh
@@ -34,13 +34,41 @@ echo "blacklist firewire-core" >> /etc/modprobe.d/firewire.conf
 echo "blacklist thunderbolt" >> /etc/modprobe.d/thunderbolt.conf
 
 
-# Regra Para liberar o SSH
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+sudo echo '[ssh]' >> /etc/fail2ban/jail.local
+sudo echo enabled = true >> /etc/fail2ban/jail.local
+sudo echo filter = sshd >> /etc/fail2ban/jail.local
+sudo echo action = iptables[name=ssh, port=”ssh”, protocol=tcp] >> /etc/fail2ban/jail.local
+sudo echo logpath = /var/log/auth.log >> /etc/fail2ban/jail.local
+sudo echo maxretry = 3 >> /etc/fail2ban/jail.local
+sudo echo bantime = 3600 >> /etc/fail2ban/jail.local
+sudo echo ignoreip = 127.0 >> /etc/fail2ban/jail.local
+
+
+sudo systemctl restart fail2ban
 
 iptables -F INPUT
 iptables -F OUTPUT
 iptables -F FORWARD
 
+#Para bloquear o SMTP
+iptables -A INPUT -p tcp --dport 25  -j DROP
+iptables -A OUTPUT -p tcp --sport 25  -j DROP
 
+#Para bloquear HTTP
+iptables -A INPUT  -p tcp --dport 80 -j DROP
+iptables -A OUTPUT -p tcp --sport 80 -j DROP
+
+# Para liberar o https
+iptables -A INPUT  -p tcp --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 443 -j ACCEPT
+
+#Para liberar o IMAP
+iptables -A INPUT -p tcp --dport 143 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 143  -j ACCEPT
+
+#Regra para liberar o ssh
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT   
 iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT   
 
